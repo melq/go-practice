b89d707a81da36b4606fce0a489fa1b5e3458002
melq <tu2sice@yahoo.co.jp>
2021-03-02 21:38:02 +0900

    :sparkles: 最大公約数をやった

---
+++ gcd/gcd.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	i, err := strconv.Atoi(next())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	x := nextInt()
+	y := nextInt()
+
+	fmt.Println(gcd(x, y))
+}
+
+func gcd(x int, y int) int {
+	for y > 0 {
+		r := x % y
+		x = y
+		y = r
+	}
+	return x
+}
