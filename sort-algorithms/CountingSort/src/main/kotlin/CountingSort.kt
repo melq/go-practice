6265a7a5d11ff5ac94821dcac7050389cd9f0014
melq <tu2sice@yahoo.co.jp>
2021-08-03 18:39:48 +0900

    :sparkles: 計数ソートをやった

--- sort-algorithms/CountingSort/src/main/kotlin/CountingSort.kt
+++ sort-algorithms/CountingSort/src/main/kotlin/CountingSort.kt
@@ -1,6 +1,35 @@
-fun main() {
-    val n = readLine()!!.toInt()
-    val list = readLine()!!.split(" ").map { it.toInt() }
+import java.util.*
 
-    println("$n $list")
+fun countingSort(arrA: IntArray, k: Int): IntArray {
+    val n = arrA.size
+    val arrC = IntArray(k)
+    for (i in 0 until k) arrC[i] = 0
+    for (i in 0 until n) arrC[arrA[i]]++
+    for (i in 0 until k) arrC[i] = arrC[i] + if (i == 0) 0 else arrC[i - 1]
+
+    val arrB = IntArray(n + 1)
+    for (i in 0 until n + 1) arrB[i] = 0
+    for (i in n - 1 downTo 0) {
+        arrB[arrC[arrA[i]]] = arrA[i]
+        arrC[arrA[i]]--
+    }
+    return arrB
+}
+
+fun IntArray.getMax(): Int {
+    var max = 0
+    for(i in this.indices)
+        if (max < this[i]) max = this[i]
+    return max
+}
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    val n = scanner.nextInt()
+    val arrA = IntArray(n)
+    for( i in 0 until n) arrA[i] = scanner.nextInt()
+
+    val arrB = countingSort(arrA, arrA.getMax() + 1)
+
+    println(arrB.joinToString(" ").substring(2))
 }
